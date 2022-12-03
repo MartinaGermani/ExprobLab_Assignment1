9819d4ee823e0e115c5bb27ac9bd19f6114b8a70
Martina Germani <91315691+MartinaGermani@users.noreply.github.com>
2022-12-03 12:32:39 +0000

    Delete assignment_exproblab directory

--- assignment_exproblab/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-setup_args = generate_distutils_setup(packages=['assignment_exproblab'], package_dir={'':'utilit>
-
-setup(**setup_args)
